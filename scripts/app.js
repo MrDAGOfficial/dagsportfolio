f840f8f8bbd357f66e920de390688fdeb32821de
DAG <developeranimatorgamer.personal@gmail.com>
2022-08-29 04:18:21 +0500

    V2.5

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,21 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 100;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
 function returnToDefault() {
     var alertBox = document.getElementById("alert-container");
     var alertTypePri = document.getElementById("alert");
@@ -89,3 +107,4 @@ function selectService() {
     }
 
 }
+
